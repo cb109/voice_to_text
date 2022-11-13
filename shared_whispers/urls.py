8a38656d793a684ea729744030d8b2c2ea3ca9cb
Christoph Buelter <buelter.christoph@gmail.com>
2022-11-13 19:23:10 +0100

    Fix missing url

--- shared_whispers/urls.py
+++ shared_whispers/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 
-from shared_whispers.core.views import api_transcribe_audio
 from shared_whispers.core.views import home
 from shared_whispers.core.views import share_target
 
 urlpatterns = [
-    path("api/transcribe/", api_transcribe_audio, name="api-transcribe"),
     path("", home, name="home"),
     path("share-target", share_target, name="share-target"),
 ]
