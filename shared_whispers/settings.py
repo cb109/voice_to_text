11df35ebaa9d5482a0a299f766ee0290e2056bd2
Christoph Buelter <buelter.christoph@gmail.com>
2022-11-13 15:23:36 +0100

    Extend manifest with icons served via whitenoise

--- shared_whispers/settings.py
+++ shared_whispers/settings.py
@@ -39,12 +39,14 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "whitenoise.runserver_nostatic",
     "django.contrib.staticfiles",
     "shared_whispers.core",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -119,6 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+STATIC_URL = "public"
 STATIC_URL = "static/"
 
 # Default primary key field type
--- shared_whispers/urls.py
+++ shared_whispers/urls.py
@@ -14,5 +14,4 @@ urlpatterns = [
     path("", transcribe_page, name="transcribe-page"),
 ]
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
