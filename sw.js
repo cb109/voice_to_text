f247701c5cb745873dbc83a7073f809dea17701a
Christoph Buelter <buelter.christoph@gmail.com>
2022-11-13 15:31:19 +0100

    Add stub serviceworker

---
+++ sw.js
@@ -0,0 +1,25 @@
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js'
+);
+
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
+
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || []);
+
+const shareTargetHandler = async ({event}) => {
+  console.log('event', event);
+  alert(event);
+
+  const formData = await event.request.formData();
+  console.log('formData', formData);
+  alert(formData);
+};
+
+workbox.routing.registerRoute(
+  '/_share-target',
+  shareTargetHandler,
+  'POST'
+);
+
+console.log('service worker initialized');
