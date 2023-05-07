7e0f87407061059324abe19a8c244c57c7a2fef4
Christoph Buelter <buelter.christoph@gmail.com>
2023-05-07 15:36:41 +0200

    Add models for original uploaded file and derived chunks

---
+++ voice_to_text/core/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+
+from voice_to_text.core.models import AudioFile
+from voice_to_text.core.models import AudioFileChunk
+
+
+class AudioFileAdmin(admin.ModelAdmin):
+    list_display = (
+        "filepath",
+        "duration",
+        "id",
+        "created_at",
+        "modified_at",
+    )
+
+
+class AudioFileChunkAdmin(admin.ModelAdmin):
+    list_display = (
+        "filepath",
+        "duration",
+        "index",
+        "original_parent_file",
+        "id",
+        "created_at",
+        "modified_at",
+    )
+
+
+admin.site.register(AudioFile, AudioFileAdmin)
+admin.site.register(AudioFileChunk, AudioFileChunkAdmin)
---
+++ voice_to_text/core/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.utils import timezone
+
+
+class TimestampMixin(models.Model):
+    """Add created and modified timestamps to a model."""
+
+    created_at = models.DateTimeField(default=timezone.now)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class AudioFile(TimestampMixin, models.Model):
+    filepath = models.CharField(max_length=512, default="", blank=True)
+
+    duration = models.FloatField(default=0.0)
+
+
+class AudioFileChunk(TimestampMixin, models.Model):
+    original_parent_file = models.ForeignKey(
+        AudioFile, on_delete=models.CASCADE, related_name="chunks"
+    )
+
+    filepath = models.CharField(max_length=512, default="", blank=True)
+
+    duration = models.FloatField(default=0.0)
+
+    index = models.PositiveIntegerField(default=0)
+
+    transcribed_text = models.TextField(default="", blank=True)
